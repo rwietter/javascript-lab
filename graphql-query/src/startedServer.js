9331188ee8fc79e3833dbcd2fc0482e06dac245d
Maurício Witter <mwitter@protonmail.com>
2020-05-24 14:41:14 -0300

    add: mongoose

---
+++ graphql-query/src/startedServer.js
@@ -0,0 +1,21 @@
+import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
+
+export default async function startServer({ typeDefs, resolvers }) {
+  const uri =
+    'mongodb+srv://euiciowr:mo5rU2sHgZV6yNc0@cluster0-yjhux.mongodb.net/week10?retryWrites=true&w=majority';
+  try {
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  server
+    .listen()
+    .then(({ url }) => console.log(`\u1F525 server starded at ${url}`));
+}
