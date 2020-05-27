e0f0287e060a0424122857dc9bc5285602ead550
Maurício Witter <mwitter@protonmail.com>
2020-05-27 15:54:28 -0300

    add: cap10 modules

---
+++ Eloquent/cap10-modules/evaluating.js
@@ -0,0 +1,20 @@
+/*
+  # Criar módulos
+  Envolver o código do módulo em uma função e usar essa função como escopo do módulo
+*/
+let plusOne = Function('n', 'return n + 1;');
+console.log(plusOne(4)); // → 5
+
+/* -------------------------------------------- */
+/* ES6 modules
+/* -------------------------------------------- */
+import ordinal from 'ordinal';
+import { days, months } from 'date-names';
+
+export function formatDate(date, format) {
+  /* ... */
+}
+/*
+ Da mesma forma, export é usada para exportar coisas. Pode aparecer na frente de uma função, classe, ou
+ definição de ligação ( let, const, ou var).
+*/
--- Eloquent/cap10-modules/readme.md
+++ Eloquent/cap10-modules/readme.md
@@ -1,1 +1,28 @@
 ## JavaScript Modules
+
+### CommonJS
+
+```javascript
+const variable = require(`module`);
+
+module.exports = variable;
+module.exports = { x, y, z };
+```
+
+---
+
+### ModulesES
+
+```javascript
+import fun from 'module';
+import fun, { foo } from 'module';
+import fun, { foo as f } from 'module;
+
+export fun;
+export default fun;
+export default ["Winter", "Spring", "Summer", "Autumn"];
+```
+
+---
+
+### Module Design
