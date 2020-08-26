e2dadccc0127945180a5e17716bc122976d33fb9
Maurício Witter <46854467+rwietter@users.noreply.github.com>
2020-08-25 22:44:38 -0300

    add: fp

---
+++ FunctionalJS/fp.js
@@ -0,0 +1,62 @@
+const logger = console.log;
+
+// --------------------- UNCURRYED ---------------------
+const getApiURL = (apiHostname, resourceName, resourceId) => {
+  return `https://${apiHostname}/api/${resourceName}/${resourceId}`;
+};
+
+const getUserURL = (userId) => {
+  return getApiURL('localhost:3000', 'users', userId);
+};
+
+const getOrderURL = (orderId) => {
+  return getApiURL('localhost:3000', 'orders', orderId);
+};
+
+const getProductURL = (productId) => {
+  return getApiURL('localhost:3000', 'products', productId);
+};
+
+// --------------------- CURRYED ---------------------
+const getAPIUrl = (hostname = 'localhost:3000') => (resourceName) => (
+  resourceId
+) => {
+  return `https://${hostname}/api/${resourceName}/${resourceId}`;
+};
+logger(getAPIUrl()('users')(2)); // https://localhost:3000/api/products/2
+logger(getAPIUrl()('orders')(2)); // https://localhost:3000/api/orders/2
+logger(getAPIUrl()('products')(2)); // https://localhost:3000/api/products/2
+
+// --------------------- Basic partial application ---------------------
+const card = [
+  { id: 1, name: 'meat', price: 24 },
+  { id: 2, name: 'egg', price: 8 },
+  { id: 3, name: 'egg', price: 12 },
+];
+
+const sum = (card, names) => {
+  const products = card.filter((el, idx, arr) => el.name === names);
+  return (value) => {
+    return products.reduce((acc, cur) => {
+      return (acc = cur.price * value);
+    }, 0);
+  };
+};
+
+const summed = sum(card, 'egg');
+logger(summed(4));
+
+// Advanced partial application
+const partialApplication = (fn, ...args) => (value) => fn(value, 5);
+
+const add = (x, y) => x + y;
+const addFive = partialApplication(add, 5);
+const result = [1, 7, 3].map(addFive);
+console.log(result); // [6, 12, 8]
+
+// --------------------- Basic composition ---------------------
+const increment = (x) => x + x;
+const double = (k) => k * k;
+
+const data = increment(double(2));
+console.log(data);
